e36d0a84059b9610b85021c894e3708bb30dea4d
karol_szot <karol_szot@o2.pl>
2020-02-25 23:15:32 +0100

    add new dependencies to project and new moduls

---
+++ model/src/main/java/com/app/model/Customer.java
@@ -0,0 +1,14 @@
+package com.app.model;
+
+
+public class Customer {
+
+    Integer id;
+    String name;
+    String surname;
+    Integer age;
+    String email;
+    Integer loyalty_card_id;
+
+
+}
